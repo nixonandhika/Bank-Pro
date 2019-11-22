d559995ea525da391f940eb7edaf14e9a2a81c77
bmusuko <bram.musuko@gmail.com>
2019-11-23 00:42:08 +0700

    integrating with soap + fix navbar

--- src/Component/Account/Account.js
+++ src/Component/Account/Account.js
@@ -1,9 +1,48 @@
 import React from 'react';
 // import Logo from './public/logo-white.png';
 import './Account.css';
-// import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 export class Account extends React.Component {
+    constructor(props){
+        super(props);
+        let account = localStorage.getItem('account_number');
+        this.state = {
+            name : '',
+            number: account,
+            balance: '',
+            loading: true
+        }
+
+    }
+
+    componentDidMount(){
+        let body = `<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ser="http://service.account.com/">
+        <soapenv:Header/>
+        <soapenv:Body>
+        <ser:getAccountByNum>
+        <!--Optional:-->
+        <arg0>${this.state.number}</arg0>
+        </ser:getAccountByNum>
+        </soapenv:Body>
+        </soapenv:Envelope>`;
+        axios.post('http://ec2-3-83-241-5.compute-1.amazonaws.com:8080/ws-bank-1.0-SNAPSHOT/services/AccountService',body,{
+            headers: {'Content-Type': 'text/xml','Access-Control-Allow-Origin': '*'}
+        })
+        .then((response)=>{
+            let xml = new DOMParser();
+            xml = xml.parseFromString(response.data,'text/xml');
+            console.log(response.data);
+            this.setState({
+                name: xml.getElementsByTagName("return")[0].getElementsByTagName('name')[0].innerHTML,
+                balance: xml.getElementsByTagName("return")[0].getElementsByTagName('balance')[0].innerHTML,
+                loading: false
+            })
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+    }
     render (){
         return(
             <div className='account'>
@@ -15,17 +54,17 @@ export class Account extends React.Component {
                         <div id='account-name' className='info-text'>
                             <label>Name</label>
                             <label>:</label>
-                            <label id='the-account-name'></label>
+                            <label id='the-account-name'>{this.state.loading ? '' : this.state.name}</label>
                         </div>
                         <div id='account-number' className='info-text'>
                             <label>Account Number</label>
                             <label>:</label>
-                            <label id='the-account-number'></label>
+                            <label id='the-account-number'>{this.state.loading ? '' : this.state.number}</label>
                         </div>
                         <div id='account-balance' className='info-text'>
                             <label>Account Balance</label>
                             <label>:</label>
-                            <label id='the-account-balance'></label>
+                            <label id='the-account-balance'>{this.state.loading ? '' : this.state.balance}</label>
                         </div>
                     </div>
                 </div>
--- src/Component/Navigation/Navigation.js
+++ src/Component/Navigation/Navigation.js
@@ -1,47 +1,75 @@
 import React from 'react';
 import Logo from '../../public/logo-white.png';
 import './Navigation.css';
-
+import {
+    Navbar, Nav, NavLink, NavbarBrand
+  } from 'reactstrap';
+  
 export class Navigation extends React.Component {
+    constructor(props){
+        super(props);
+        let account = localStorage.getItem('account_number');
+        if(account === null){
+            window.alert('Login First');
+            window.location.replace("/login");
+          }  
+        this.handleLogout = this.handleLogout.bind(this);
+    }
+
+    handleLogout(){
+        localStorage.removeItem('account_number');
+        alert('You logout');
+        window.location.replace('/');
+    }
     render() {
         return (
-            <div className='account-navigation'>
-                <div className='logo'>
-                    <a href='/login'>
-                    <div className='logo-img'>
-                        <img id='logo-pic' src={Logo} alt='bank-logo'/>
-                    </div>
-                    </a>
-                    <a style={{'textDecoration': 'none'}}href='/login'>
-                    <div className='logo-text'>
-                        <label id='account-com-name'>Bank Pro</label>
-                        <label id='account-com-motto'>Your #1 Partner</label>
-                    </div>
-                    </a>
-                </div>
-                <div className='link'>
-                    <a style={{'textDecoration': 'none'}} href='/account'>
-                        <div className='box'>
-                            <a style={{color:'white','textDecoration': 'none'}} href='/account'>Account</a>
-                        </div>
-                    </a>
-                    <a style={{'textDecoration': 'none'}} href='/transfer'>
-                        <div className='box'>
-                            <a style={{color:'white','textDecoration': 'none'}} href='/transfer'>Transfer</a>
-                        </div>
-                    </a>
-                    <a style={{'textDecoration': 'none'}} href='/history'>
-                        <div className='box'>
-                            <a style={{color:'white','textDecoration': 'none'}} href='/history'>History</a>
-                        </div>
-                    </a>
-                    <a style={{'textDecoration': 'none'}} href='/'>
-                        <div className='box'>
-                            <a style={{color:'white','textDecoration': 'none'}} href='/'>Logout</a>
-                        </div>
-                    </a>
-                </div>
-            </div>
+            // <div className='account-navigation'>
+            //     <div className='logo'>
+            //         <a href='/login'>
+            //         <div className='logo-img'>
+            //             <img id='logo-pic' src={Logo} alt='bank-logo'/>
+            //         </div>
+            //         </a>
+            //         <a style={{'textDecoration': 'none'}}href='/login'>
+            //         <div className='logo-text'>
+            //             <label id='account-com-name'>Bank Pro</label>
+            //             <label id='account-com-motto'>Your #1 Partner</label>
+            //         </div>
+            //         </a>
+            //     </div>
+            //     <div className='link'>
+            //         <a style={{'textDecoration': 'none'}} href='/account'>
+            //             <div className='box'>
+            //                 <a style={{color:'white','textDecoration': 'none'}} href='/account'>Account</a>
+            //             </div>
+            //         </a>
+            //         <a style={{'textDecoration': 'none'}} href='/transfer'>
+            //             <div className='box'>
+            //                 <a style={{color:'white','textDecoration': 'none'}} href='/transfer'>Transfer</a>
+            //             </div>
+            //         </a>
+            //         <a style={{'textDecoration': 'none'}} href='/history'>
+            //             <div className='box'>
+            //                 <a style={{color:'white','textDecoration': 'none'}} href='/history'>History</a>
+            //             </div>
+            //         </a>
+            //         <a style={{'textDecoration': 'none'}} href='/'>
+            //             <div className='box'>
+            //                 <a style={{color:'white','textDecoration': 'none'}} onClick={this.handleLogout} href='/'>Logout</a>
+            //             </div>
+            //         </a>
+            //     </div>
+            // </div>
+            <Navbar>
+                <NavbarBrand href="/" style={{color : '#FFFFFF', fontSize:30, fontWeight:"bold", fontStyle:'italic'}}><img id='logo-pic' src={Logo} alt='bank-logo' style={{'width':'5rem'}}/>Bank Pro</NavbarBrand>
+                <Nav>
+                    <NavLink href="/account" style={{color : 'white'}}>Account</NavLink>
+                    <NavLink href="/transfer" style={{color : 'white'}}>Transfer</NavLink>
+                    <NavLink href="/history" style={{color : 'white'}}>History</NavLink>
+                    <NavLink href="/" onClick={this.handleLogout} style={{color : 'white'}}>Logout</NavLink>
+                </Nav>
+
+            </Navbar>
         )
     }
 }
