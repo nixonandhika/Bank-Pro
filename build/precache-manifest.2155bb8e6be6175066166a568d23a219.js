cf617e947441fc139c0acc84eea1dc70462c2c99
bmusuko <bram.musuko@gmail.com>
2019-11-24 17:10:43 +0700

    build and .gitlab not working

---
+++ build/precache-manifest.2155bb8e6be6175066166a568d23a219.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20248d025ca0aa8bed07d5773556e728",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21fe3ac8aaaf6aaf5587",
+    "url": "/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "0420da1b8a53cb0d66bb",
+    "url": "/static/css/main.8cc647bd.chunk.css"
+  },
+  {
+    "revision": "21fe3ac8aaaf6aaf5587",
+    "url": "/static/js/2.6663674c.chunk.js"
+  },
+  {
+    "revision": "0420da1b8a53cb0d66bb",
+    "url": "/static/js/main.fd5bf812.chunk.js"
+  },
+  {
+    "revision": "a3fbcff33a4c9cc4a177",
+    "url": "/static/js/runtime-main.fa09f4e5.js"
+  },
+  {
+    "revision": "bfd747ef2df17d60abd50970a0a81d00",
+    "url": "/static/media/homepage-pic.bfd747ef.png"
+  },
+  {
+    "revision": "8b87e71f0c7561507c24d4fcacc60a0b",
+    "url": "/static/media/logo-white.8b87e71f.png"
+  },
+  {
+    "revision": "2bfa88e1e26b8c55a60dd1a4a91bf988",
+    "url": "/static/media/logo.2bfa88e1.png"
+  }
+]);
